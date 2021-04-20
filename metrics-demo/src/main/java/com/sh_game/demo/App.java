728347beaf51fbce5f402835058e17721f3dd749
SilenceSu <silence.sx@gmail.com>
2021-04-20 21:00:31 +0800

    1.修改父类版本。 2.修改实现类scope 3.demo引用修改

--- metrics-demo/src/main/java/com/sh_game/demo/App.java
+++ metrics-demo/src/main/java/com/sh_game/demo/App.java
@@ -1,16 +1,22 @@
 package com.sh_game.demo;
 
 import com.sh_game.metrics.core.Metrics;
-import com.sh_game.metrics.core.registry.DefaultPrometheus;
 import io.micrometer.core.instrument.Counter;
+
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println("demo");
-        DefaultPrometheus.metrics_prot = 3334;
-        Counter counted = Metrics.meter().counter("name.dd.ad");
+        Counter counted = Metrics.counter("name.dd.ad");
         counted.increment();
+
+
+        Map<String, String> configs = Metrics.getResourceConfig().getAll();
+        for (Map.Entry<String, String> entry : configs.entrySet()) {
+            System.out.println("当前使用配置 key:" + entry.getKey() + ",value={" + entry.getValue() + "}");
+        }
 
 
     }
